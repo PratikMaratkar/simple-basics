cffe440aaa422ce40954b199c1bd01c00288cbba
BeingHuman <maratkarpratik50@gmail.com>
2021-03-03 22:38:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import About from './component/About.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="myclass">
+        Hello World using by class css method
+      </h1>
+      
+      <h2>
+        hello World using by tag css method
+      </h2> 
+
+      <p id="ID">
+        Hello World using by ID css method
+      </p>
+<About></About>
+<About></About>
+<About></About>
     </div>
+  
+  
   );
 }
 
---
+++ src/component/About.js
@@ -0,0 +1,14 @@
+import'./About.css';
+
+function About() {
+    return (
+      <div>
+        <h1>this is basic component program</h1>
+  
+      </div>
+    
+    
+    );
+  }
+  
+  export default About;
